281709ed532585db96d7745df396ebccc14c1e36
CORP\Neeharika.Pathuri <neehu395@gmail.com>
2017-01-23 22:52:08 +0530

    no message

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic_class
+{
+    class Program
+    {
+        class mycollection<T>
+        {
+            private int Length;
+            public mycollection(int Lenght)
+            {
+                this.Length = Length;
+            }
+            public string this [int length]
+            {
+                
+                get
+                    {
+                    int count2, index;
+                    if (index >= 0 && index <= size - 1)
+                    {
+                        count2 = namelist[index];
+
+                        return count2;
+                    }
+                }
+            }
+            
+            
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
